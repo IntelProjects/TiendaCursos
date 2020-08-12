52fa6546405707242f827ad647f6da63a820695a
Fernando Contreras <fernandocontreras@MacBook-Pro-de-Fernando.local>
2020-08-12 12:04:14 -0500

    cambio de nombre

---
+++ theme/assets/corporate/scripts/carrito.js
@@ -0,0 +1,10 @@
+class Carrito{
+    
+    //Añadir el producto al carrito
+    comprarProducto(e){
+     e.preventDefault();
+     if(e.target.classList.contains('add2cart')){
+        
+     }
+    }
+}
